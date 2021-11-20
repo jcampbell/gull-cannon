6f624e5fe892ac8958a6d54313f8ca6c0eb2c21e
James Campbell <james.p.campbell@gmail.com>
2021-11-19 21:58:10 -0500

    Add cloud function

---
+++ cloud-function/main.py
@@ -0,0 +1,42 @@
+import os
+import logging
+
+from sqlalchemy.engine import create_engine
+
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import UUID, JSONB, TIMESTAMP
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.sql import func
+from sqlalchemy_cockroachdb import run_transaction
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+Base = declarative_base()
+connection_string = os.environ.get("CONNECTION_STRING")
+if not connection_string:
+    logger.error("No connection string available; please set CONNECTION_STRING environment variable.")
+    logger.error("Using ephemeral database")
+    exit()
+
+engine = create_engine(connection_string)
+
+
+class Checkin(Base):
+    __tablename__ = 'checkins'
+    id = Column(UUID(as_uuid=True), primary_key=True, server_default=func.uuid_generate_v4())
+    datetime = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    headers = Column(JSONB)
+    payload = Column(JSONB)
+
+
+def handler(request):
+    headers = dict(request.headers())
+    payload = request.json()
+    checkin = Checkin(headers=headers, payload=payload)
+
+    run_transaction(sessionmaker(bind=engine), lambda s: s.add(checkin))
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,24 +9,100 @@ terraform {
 }
 
 provider "google" {
-  project     = "gullcannon"
-  region      = "us-east1"
+  alias   = "baserate"
+  project = "warm-drive-332522"
+  region  = "us-east1"
+}
+
+provider "google" {
+  project      = module.project-factory.project_id
+  region       = "us-east-1"
+  access_token = data.google_service_account_access_token.default.access_token
+}
+
+data "google_service_account_access_token" "default" {
+  provider               = "google.baserate"
+  target_service_account = module.project-factory.api_s_account_fmt
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "300s"
 }
 
 module "project-factory" {
-  source  = "terraform-google-modules/project-factory/google"
-  version = "~> 10.1"
+  providers = {
+    google = google.baserate
+  }
+  source    = "terraform-google-modules/project-factory/google"
+  version   = "~> 10.1"
 
-  name                 = "gull-cannon"
-  random_project_id    = true
-  org_id               = "1077903016582"
-  billing_account      = "0167E9-D67CAC-E98E9B"
+  name              = "gull-cannon"
+  random_project_id = true
+  org_id            = "1077903016582"
+  billing_account   = "0167E9-D67CAC-E98E9B"
 
   activate_apis = ["cloudtasks.googleapis.com", "cloudfunctions.googleapis.com"]
   # , "pubsub.googleapis.com", "logging.googleapis.com",
 }
 
-variable "GOOGLE_CREDENTIALS" {  # PLACEHOLDER --> this should never be used
-  type                 = string
-  sensitive            = true
+locals {
+  root_dir  = abspath("../cloud-function")
+  timestamp = formatdate("YYMMDDhhmmss", timestamp())
+}
+
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = local.root_dir
+  output_path = "/tmp/function-${local.timestamp}.zip"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "project-bucket"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "handler.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = data.archive_file.source.output_path
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "gull-cannon"
+  description = "gull-cannon logger"
+  runtime     = "python39"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "handler"
+  environment_variables = {
+    CONNECTION_STRING = var.CONNECTION_STRING
+  }
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+variable "CONNECTION_STRING" {
+  type      = string
+  sensitive = true
+}
+
+output "trigger_url" {
+  value = google_cloudfunctions_function.function.https_trigger_url
+}
+
+output "project_name" {
+  value = module.project-factory.project_name
+}
+
+output "project_id" {
+  value = module.project-factory.project_id
 }
