67a9a35dde77695f90d387a6018b254251c34788
James Campbell <james.p.campbell@gmail.com>
2021-11-20 00:45:54 -0500

    abandon opinionation

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,11 +1,3 @@
 output "trigger_url" {
   value = google_cloudfunctions_function.function.https_trigger_url
 }
-
-output "project_name" {
-  value = module.project-factory.project_name
-}
-
-output "project_id" {
-  value = module.project-factory.project_id
-}
