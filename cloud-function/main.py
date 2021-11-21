a91a0ca07075b58e1a9420ff9c4da4f61ae1d4fd
James Campbell <james.p.campbell@gmail.com>
2021-11-20 23:24:10 -0500

    Tokens

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -1,12 +1,15 @@
 import os
 import logging
+import uuid
+import random
 
 import flask
 from sqlalchemy.engine import create_engine
 
-from sqlalchemy import Column
-from sqlalchemy.dialects.postgresql import UUID, JSONB, TIMESTAMP
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, CHAR, BOOLEAN, ForeignKey, select, BIGINT, update
+from sqlalchemy.dialects.postgresql import UUID, JSONB, TIMESTAMP, TEXT
+from sqlalchemy.exc import MultipleResultsFound, NoResultFound
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from sqlalchemy.sql import func
 from sqlalchemy_cockroachdb import run_transaction
 
@@ -20,7 +23,7 @@ if not connection_string:
     exit()
 
 engine = create_engine(connection_string)
-
+Session = sessionmaker(engine)
 
 class Checkin(Base):
     __tablename__ = 'checkins'
@@ -30,16 +33,86 @@ class Checkin(Base):
     payload = Column(JSONB)
 
 
+class User(Base):
+    __tablename__ = 'users'
+    username = Column(TEXT, primary_key=True)
+    token = Column(CHAR(40), index=True)
+
+
+RECOGNIZED_ACTIONS = {"fire", "sleep"}
+
+
+class Action(Base):
+    __tablename__ = 'actions'
+    id = Column(UUID(as_uuid=True), primary_key=True, server_default=func.uuid_generate_v4(), index=True)
+    username = Column(TEXT, ForeignKey("users.username"))
+    user = relationship(User)
+    datetime = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    action = Column(TEXT, nullable=False)
+    duration = Column(BIGINT, default=1000)
+    completed = Column(BOOLEAN, default=False)
+
+
 def handler(request):
+    is_valid, message = check_token(request)
+    if not is_valid:
+        return message, 404
+
+    user : User = message.get("user")
     headers = dict(request.headers)
     payload = request.json
     checkin = Checkin(headers=headers, payload=payload)
 
-    run_transaction(sessionmaker(bind=engine), lambda s: s.add(checkin))
+    run_transaction(Session, lambda s: s.add(checkin))
+    action_requests = payload.get("action_requests", [])
+    action_updates = payload.get("action_updates", [])
+    for action_request in action_requests:
+        action = action_request.get("action", "noop")
+        if action in RECOGNIZED_ACTIONS:
+            action_obj = Action(
+                username = user.username,
+                action=action,
+                duration=action_request.get("params", {}).get("duration", 1000),
+                dispatched=False
+            )
+            run_transaction(Session, lambda s: s.add(action_obj))
+
+    for action_update in action_updates:
+        try:
+            id = uuid.UUID(action_update.get("id"))
+            completed = action_update.get("completed", True)
+        except ValueError as e:
+            return {"error": str(e)}
+        run_transaction(Session, lambda s: s.execute(update(Action).where(Action.id == id).vaalues(completed=completed)))
+
     return flask.jsonify({
-        "sleep": 10000,
-        "fire": False
+        "action_requests": [
+            {
+                "action": "sleep",
+                "params": {
+                    "duration": random.randint(8, 12)
+                }
+            }
+        ]
     })
+
+
+def check_token(request):
+    logger.debug("Getting authentication token")
+    try:
+        bearer = request.headers.get("Authorization")
+        token = bearer.split()[1]
+    except IndexError:
+        return False, {
+            "error": "unable to parse authorization token"
+        }
+
+    with Session() as session:
+        try:
+            user = session.execute(select(User).where(User.token == token)).scalar_one()
+            return True, {"user": user}
+        except (NoResultFound, MultipleResultsFound):
+            return False, {"error": "unrecognized token"}
 
 
 if __name__ == "__main__":
---
+++ cloud-function/test_main.py
@@ -0,0 +1,27 @@
+import uuid
+
+import pytest
+from sqlalchemy import select
+
+from main import Session, Checkin
+
+
+def test_example():
+    expected_id = uuid.UUID("4a91a4bc-d363-420b-b110-26bd74b9b9fa")
+    with Session() as session:
+        res = session.execute(
+            select(Checkin).where(Checkin.id == expected_id)
+        ).scalar_one()
+
+    assert res.id == expected_id
+
+
+def test_uuid():
+    with pytest.raises(ValueError) as exc:
+        _ = uuid.UUID("a;lkasdf")
+    assert "badly formed hexadecimal UUID string" in str(exc.value)
+
+
+def test_random():
+    import random
+    assert 8 <= random.randint(8, 12) <= 12
