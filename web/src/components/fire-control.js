a9fe83ea890316494aef92b3cd9df3191927d341
James Campbell <james.p.campbell@gmail.com>
2021-12-01 08:53:42 -0500

    UI Beautification

--- web/src/components/fire-control.js
+++ web/src/components/fire-control.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {Button, Row, Col, Input, InputNumber, Slider, Typography, Tabs, message, Table} from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 
 const {Text, Paragraph} = Typography;
 const {TabPane} = Tabs;
@@ -13,7 +14,8 @@ class FireControl extends React.Component {
             checkins: undefined,
             delay: undefined,
             fireDuration: 60,
-            token: undefined
+            token: undefined,
+            isLoading: false,
         }
     };
 
@@ -34,6 +36,7 @@ class FireControl extends React.Component {
             message.error('Token is required.');
             return;
         }
+        this.setState({isLoading: true});
         const requestOptions = {
             method: 'GET',
             headers: {'Authorization': 'Bearer ' + this.state.token},
@@ -41,10 +44,15 @@ class FireControl extends React.Component {
             // body: JSON.stringify({ title: 'React POST Request Example' })
         };
         fetch('https://us-east1-baserate-332800.cloudfunctions.net/gull-cannon/checkins', requestOptions)
-            .then(response => response.json())
-            .then(data => this.setState({checkins: data.checkins}))
+            .then(response => {
+                this.setState({isLoading: false});
+                if (!response.ok) {
+                    throw new Error("Error: " + response.status);
+                }
+                else return response.json()
+            })            .then(data => this.setState({checkins: data.checkins}))
             .catch((error) => {
-                console.error('Error:', error);
+                message.error(error.message);
             });
     }
 
@@ -53,15 +61,21 @@ class FireControl extends React.Component {
             message.error('Token is required');
             return;
         }
+        this.setState({isLoading: true});
         const requestOptions = {
             method: 'GET',
             headers: {'Authorization': 'Bearer ' + this.state.token},
         };
         fetch('https://us-east1-baserate-332800.cloudfunctions.net/gull-cannon/actions', requestOptions)
-            .then(response => response.json())
-            .then(data => this.setState({actions: data.actions, delay: data.delay}))
+            .then(response => {
+                this.setState({isLoading: false});
+                if (!response.ok) {
+                    throw new Error("Error: " + response.status);
+                }
+                else return response.json()
+            })            .then(data => this.setState({actions: data.actions, delay: data.delay}))
             .catch((error) => {
-                console.error('Error:', error);
+                message.error(error.message);
             });
     };
 
@@ -70,16 +84,26 @@ class FireControl extends React.Component {
             message.error('Token is required');
             return;
         }
+        this.setState({isLoading: true});
         const requestOptions = {
             method: 'POST',
             headers: {'Authorization': 'Bearer ' + this.state.token, 'Content-Type': 'application/json' },
             body: JSON.stringify({ action: "fire", "duration": this.state.fireDuration * 1000 })
         };
         fetch('https://us-east1-baserate-332800.cloudfunctions.net/gull-cannon/actions', requestOptions)
-            .then(response => response.json())
-            .then(data => this.setState({actions: data.actions, delay: data.delay}))
+            .then(response => {
+                this.setState({isLoading: false});
+                if (!response.ok) {
+                    throw new Error("Error: " + response.status);
+                }
+                else return response.json()
+            })
+            .then(data => {
+                this.setState({actions: data.actions, delay: data.delay});
+                message.success("Successfully created fire request.");
+            })
             .catch((error) => {
-                console.error('Error:', error);
+                message.error(error.message);
             });
     }
 
@@ -111,7 +135,7 @@ class FireControl extends React.Component {
         ]
         return (
             <>
-                <Input placeholder="token" value={this.state.token} onChange={e => this.onTokenChange(e)}/>
+                <Input.Password placeholder="token" value={this.state.token} onChange={e => this.onTokenChange(e)} iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}/>
                 <Tabs defaultActiveKey="1">
                     <TabPane tab="Request Fire" key="1">
                         <Text>Request gull cannon to fire for the specified duration (in seconds).</Text>
